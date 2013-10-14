10c76f4e7b0efb0383e38ecb396616de084cb147
Ethan-Zhang <networm2@163.com>
2013-10-14 11:28:30 +0800

    BUG FIX: fishing script
    Fix an error in fishing script, when create a new project directory, the fish
    script walk through the top dir instead of the porject dir to modify

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     packages=find_packages(),
     package_data={'':['*.ico']},
     include_package_data=True,
-    version='1.4.0',
+    version='1.4.1',
     scripts=['mownfish/bin/mownfish'],
     py_modules=['mownfish'],
     )
