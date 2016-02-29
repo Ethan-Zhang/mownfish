e046265b6fe7ab6ef8dfe196c902d76dbec42a2b
Ethan-Zhang <networm2@163.com>
2016-02-29 14:49:38 +0800

    Modify: version 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ setup(
     author_email = 'unpeeled_onion@outlook.com',
     license="http://www.apache.org/licenses/LICENSE-2.0",
     url = 'https://github.com/ethan-zhang/mownfish',
-    download_url = 'https://github.com/Ethan-Zhang/mownfish/tree/v1.0',
+    download_url = 'https://github.com/Ethan-Zhang/mownfish/tree/v2.4',
     keywords = ['tornado'],
     packages=find_packages(),
     package_data={'':['*.ico']},
     include_package_data=True,
-    version='2.4.1',
+    version='2.4.2',
     entry_points = {
         'console_scripts': [
             'mownfishd = mownfish.cmd.mownfishd:main',
