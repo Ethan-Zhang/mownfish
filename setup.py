6923416a6ef64f0a4b6df7a4a096e955512608f4
Ethan-Zhang <networm2@163.com>
2016-03-07 18:17:37 +0800

    Modify: setup 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ setup(
     author_email = 'unpeeled_onion@outlook.com',
     license="http://www.apache.org/licenses/LICENSE-2.0",
     url = 'https://github.com/ethan-zhang/mownfish',
-    download_url = 'https://github.com/Ethan-Zhang/mownfish/tree/v2.4',
+    download_url = 'https://github.com/Ethan-Zhang/mownfish/tree/v3.1',
     keywords = ['tornado'],
     packages=find_packages(),
     include_package_data=True,
-    version='3.1.0',
+    version='3.1.1',
     entry_points = {
         'console_scripts': [
             'fishing = mownfish.cmd.fishing:main',
